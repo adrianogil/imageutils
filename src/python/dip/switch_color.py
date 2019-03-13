bb79fe347c372051db1cb602c42bb74296252f1c
Adriano Gil <adrianogil.san@gmail.com>
2019-03-12 20:11:04 -0400

    Added inverse mode to switch-color tool

--- src/python/dip/switch_color.py
+++ src/python/dip/switch_color.py
@@ -6,6 +6,13 @@ output_image = sys.argv[2]
 original_color_str = sys.argv[3]
 new_color_str = sys.argv[4]
 
+flags = []
+if len(sys.argv) >= 5:
+    for i in range(5, len(sys.argv)):
+        flags.append(sys.argv[i])
+
+inversed_mode = "-i" in flags
+
 
 def convert_from_str(list_str):
     list_data = []
@@ -29,14 +36,17 @@ for x in range(0, width):
     for y in range(0, height):
         color = pixels[x, y]
 
-        print(color)
+        should_switch_color = color[0] == original_color[0] and \
+            (len(original_color) <= 1 or color[1] == original_color[1]) and \
+            (len(original_color) <= 2 or color[2] == original_color[2]) and \
+            (len(original_color) <= 3 or color[3] == original_color[3])
 
-        if color[0] == original_color[0] and \
-                (len(original_color) <= 1 or color[1] == original_color[1]) and \
-                (len(original_color) <= 2 or color[2] == original_color[2]) and \
-                (len(original_color) <= 3 or color[3] == original_color[3]):
+        if inversed_mode:
+            should_switch_color = not should_switch_color
+
+        if should_switch_color:
             color = tuple(new_color)
             pixels[x, y] = color
-            # print('replaced color')
+        # print('replaced color')
 
 image.save(output_image)
