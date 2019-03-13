01efb8988a8d7fa89edbbec9d1df5a5353d20ef0
Adriano Gil <adrianogil.san@gmail.com>
2019-03-12 20:12:57 -0400

    Added new tools to bashrc

--- src/bashrc.sh
+++ src/bashrc.sh
@@ -1,3 +1,5 @@
 alias dip-list-images='python2 $IMAGE_UTILS_DIR/list_images.py'
 alias dip-switch-color='python3 $IMAGE_UTILS_DIR/python/dip/switch_color.py'
 alias dip-switch-color-dist='python3 $IMAGE_UTILS_DIR/python/dip/switch_color_distance.py'
+alias dip-histogram='python3 $IMAGE_UTILS_DIR/python/dip/color_histogram.py'
+alias dip-crop='python3 $IMAGE_UTILS_DIR/python/dip/crop.py'
