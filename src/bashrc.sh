b1757a589fb6bcc33839ff521fac417e06a08e5c
Adriano Gil <adrianogil.san@gmail.com>
2019-09-26 18:46:10 -0400

    Added new tools

--- src/bashrc.sh
+++ src/bashrc.sh
@@ -5,6 +5,15 @@ alias dip-histogram='python3 $IMAGE_UTILS_DIR/python/dip/color_histogram.py'
 alias dip-crop='python3 $IMAGE_UTILS_DIR/python/dip/crop.py'
 alias dip-grid='python3 $IMAGE_UTILS_DIR/python/dip/grid.py'
 
+function dip-background-transparent()
+{
+    target_image=$1
+    output_image=$2
+
+    convert ${target_image} -transparent white ${output_image}
+}
+
+
 # @tool dip-show-colors-in-point-cloud shows an image as a color point cloud in RGB space
 # You need to setup first a html hosting at repo folder
 # https://github.com/yig/image-rgb-in-3D
@@ -16,4 +25,30 @@ function dip-show-colors-in-point-cloud()
     cp $target_image $COLORS_IN_POINT_CLOUD_DIR/target_images/
 
     open http://localhost:5353?image=target_images/$target_image
+}
+
+function dip-gif-frame-reduce()
+{
+    # This script will take an animated GIF and delete every other frame
+    # Accepts two parameters: input file and output file
+    # Usage: ./<scriptfilename> input.gif output.gif
+
+    # Make a copy of the file
+    cp $1 $2
+
+    # Get the number of frames
+    numframes=`gifsicle $1 -I | ggrep -P "\d+ images" --only-matching | ggrep -P "\d+" --only-matching`
+
+    # Deletion
+    let i=0
+    while [[ $i -lt $numframes  ]]; do
+        rem=$(( $i % 2 ))
+
+        if [ $rem -eq 0 ]
+        then
+            gifsicle $2 --delete "#"$(($i/2)) -o $2
+        fi
+
+        let i=i+1
+    done
 }
--- src/list_images.py
+++ src/list_images.py
@@ -1,16 +1,12 @@
-import sys, os
-
+import os
+import sys
 import fnmatch
 from os.path import join
-
 from PIL import Image
 
 input_path = sys.argv[1]
 
-if len(sys.argv) >= 3 and sys.argv[2] == '-v':
-    verbose_mode = True
-else:
-    verbose_mode = False
+verbose_mode = '-v' in sys.argv
 
 all_images = {}
 
@@ -22,11 +18,14 @@ def parse_file(file_path):
 
     # all_images.append(, file))
     if resolution_str in all_images:
-        cur_list = all_images[resolution_str]
+        cur_list = all_images[resolution_str]["images"]
         cur_list.append(file_path)
-        all_images[resolution_str] = cur_list
     else:
-        all_images[resolution_str] = [file_path]
+        all_images[resolution_str] = {
+            "width": width,
+            "height": height,
+            "images": [file_path]
+        }
 
 def parse_project(project_path, file_extension):
     for root, subFolders, files in os.walk(project_path):
@@ -36,16 +35,25 @@ def parse_project(project_path, file_extension):
 
 parse_project(input_path, ('.jpg', '.jpeg', '.gif', '.png'))
 
-for i in all_images:
-    print(i + ":  (" + str(len(all_images[i])) + ' images)' )
-    for a in all_images[i]:
-        if verbose_mode:
+sorted_all_images = sorted(all_images.keys(), key=lambda x: len(all_images[x]["images"]), reverse=True)
+
+mean_width = 0
+mean_height = 0
+total = 0
+
+for i in sorted_all_images:
+    number_images = len(all_images[i]["images"])
+    print(i + ":  (" + str(number_images) + ' images)')
+
+    mean_height += number_images * all_images[i]["height"]
+    mean_width += number_images * all_images[i]["width"]
+    total += number_images
+
+    if verbose_mode:
+        for a in all_images[i]["images"]:
             print('\t\t' + a)
 
-# def get_key(item):
-#     return int(item[0])
+mean_width = mean_width * 1.0 / total
+mean_height = mean_height * 1.0 / total
 
-# images_count = sorted(branches_count, key=get_key, reverse=True)
-
-# for b in branches_count:
-#     print(b[0] + ": " + b[1])
+print("Mean size: %sx%s" % (mean_width, mean_height))
